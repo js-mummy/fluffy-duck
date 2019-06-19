8d9fea567a0e2b568cd7b4d4923fce1839c7c567
Alexander <alexander.sergienko@gmail.com>
2019-06-19 05:24:05 +0300

    Тестовое задание 008.1

--- Project 1/myjs.js
+++ Project 1/myjs.js
@@ -1,11 +1,18 @@
-function isEven(data) {
-  function nMinusDva(schet) {
-    if ((Math.abs(data) - schet) == 0) return true;
-    else if ((Math.abs(data) - schet) == 1) return false;
-    else
-        console.log(schet);
-        return nMinusDva(schet+2);
-  }
-  return nMinusDva(0);
-}
-console.log(isEven(10));
+var money,
+    time;
+
+var expName = prompt ("Введите обязательную статью расходов в этом месяце");
+var expData = +prompt ("Во сколько обойдется?");
+
+var appData = {
+  budget : money,
+  timeData : time,
+  expenses : {expName1 : expName, expData1 : expData},
+  optionalExpenses : "",
+  income : [],
+  savings : false
+};
+
+//alert(expData/30);
+console.log(appData.expenses.expName1);
+console.log(appData.expenses.expData1/30);
