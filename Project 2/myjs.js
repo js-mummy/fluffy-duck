8e67b8b2c9d148b30dace392a91d86732481ca62
Alexander <alexander.sergienko@gmail.com>
2019-06-21 04:51:37 +0300

    Оформление задания с помощью функций

--- Project 2/myjs.js
+++ Project 2/myjs.js
@@ -9,38 +9,47 @@
 -- Операторы условия, возвращают true Или false
 */
 
-let money = +prompt("Ваш бюджет на месяц?", ''),
-    time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
-let appData = {
-  budget : money,
-  timeData : time,
-  expenses : {},
-  optionalExpenses : {},
-  income : [],
-  savings : false
-};
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt ("Введите обязательную статью расходов в этом месяце" + i, ''),
-        b = prompt ("Во сколько обойдется?" + i, '');
-        
-        console.log(typeof(a));
-        console.log(typeof(b));
-    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-    && a != '' && b != '' && a.length < 50) {
+var p = function detectDayBudget() {
+
+  var money = +prompt("Ваш бюджет на месяц?", ''),
+  time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+  let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+  };
+
+  for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце" + i, ''),
+      b = prompt("Во сколько обойдется?" + i, '');
+
+    console.log(typeof (a));
+    console.log(typeof (b));
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
+      a != '' && b != '' && a.length < 50) {
       console.log("done" + i);
       appData.expenses[a] = b;
     } else {
       do {
-        a = prompt ("Введите обязательную статью расходов в этом месяце" + i+1, '');
-        b = prompt ("Во сколько обойдется?" + i+1, '');
+        a = prompt("Введите обязательную статью расходов в этом месяце" + i + 1, '');
+        b = prompt("Во сколько обойдется?" + i + 1, '');
       }
-      while(!a && !b);
+      while (!a && !b);
       appData.expenses[a] = b;
     }
-}
+  }
 
-appData.moneyPerDay = appData.budget / 30;
+  appData.moneyPerDay = appData.budget / 30;
 
-alert("Дневной бюджет: " + appData.moneyPerDay);
+  alert("Дневной бюджет: " + appData.moneyPerDay);
+
+  console.log(appData);
+};
+
+p();
