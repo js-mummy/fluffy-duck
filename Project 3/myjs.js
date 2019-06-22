376d56909a2acce3008af6b8a1d4e6b4588e4fa5
Alexander <alexander.sergienko@gmail.com>
2019-06-22 21:32:10 +0300

    call-back function

--- Project 3/myjs.js
+++ Project 3/myjs.js
@@ -1,15 +1,14 @@
-function first(x, z) {
+function first(x, callback) {
   
   console.log((x*100)/200*30+20-70+35);
 
-    z();
+    callback();
 }
 
 function second(a,b) {
   console.log(a*b);
 }
 
-
-
 first(1000, function(){
-  second(5,6)});
+  second(5,6);
+});
