03d8a0ce5be6724d7d29836b141e636178943d49
Alexander <alexander.sergienko@gmail.com>
2019-06-23 03:01:11 +0300

    push map multidimensional array

--- Lesson 12/myjs.js
+++ Lesson 12/myjs.js
@@ -1,19 +1,59 @@
 // Map array
-let testArray = [1, 20, 'Test', true];
+/*let testArray = [1, 20, 'Test', true];
 testArray[6] = 18;
 
 let d = testArray.map(function(x){
   return x*5;
 });
 
-console.log(d);
+console.log(d);*/
+/*
 
+// Push and Shift for arrays
 let tempArray = [];
-let c = '';
 
 for (let i = 10; i >= 0; i--) {
-  if (tempArray.length > 8) { break; }
+  if (i % 2 == 0) { continue; }
   tempArray.push(i);
 }
 
 console.log(tempArray);
+
+
+var fruits = []; // создать массив
+
+fruits.age = 25; // свойство со строковым именем
+fruits[99999] = 5; // присвоить свойство с любым номером
+console.log(fruits);
+fruits.length = 0; // Очищаем массив
+console.log(fruits);
+
+let arr = [9999,3,{face:'beauty'}];
+
+arr.push("My");
+console.log(arr.length);
+console.log(arr);
+console.log(typeof(arr[4]));*/
+
+
+
+
+
+var matrix = [
+   [1, 2, 3, 0, 0],
+   [4, 5, 6, 0, 0],
+   [7, 8, 9, 0, 0],
+   [10, 11, 12, 0, 0]
+ ];
+ console.log(matrix[0].length);
+ console.log(matrix[0].length);
+
+ let s = 0;
+for (let i = 0; i < matrix.length; i++) {
+    for (let k = 0; k < matrix[i].length; k++) {
+      s += matrix[i][k];
+    }
+    //console.log(s);
+}
+
+console.log(s);
